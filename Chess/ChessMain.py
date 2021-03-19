ac5426d8c52dada9f3bd2099378b35d613dc29ca
giovannfox <gfox@mail.sfsu.edu>
2021-03-18 18:01:04 -0700

    Added main function with initialization of pygame as well as screen, clock  and game state variables

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -20,4 +20,30 @@ Initialize a global dictionary of images. This is called exactly once in main.
 def load_images():
     pieces = ['wp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bp', 'bR', 'bK', 'bB', 'bQ', 'bK']
     for piece in pieces:
-        IMAGES[piece] = p.image.load("images/" + piece + ".png")
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    # Note: we can access an image by saying 'IMAGES['wp']'
+    # p.transform.scale will make images the right size
+
+
+"""
+This will be the main driver for the code.  This will handle user input and and updating the graphics
+"""
+
+
+def main():
+    # initialize pygame
+    p.init()
+    # screen variable
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    # clock variable
+    clock = p.time.Clock()
+    # fill screen with color white, not necessary later on
+    screen.fill(p.Color("white"))
+    # create game state, gives access to variables in ChessEngine like board for example
+    gs = ChessEngine.GameState()
+
+    # example
+    print(gs.board)
+
+
+main()
