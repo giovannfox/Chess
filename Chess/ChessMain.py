29057cdead6aceb804a7fa97a17013c812288b45
giovannfox <gfox@mail.sfsu.edu>
2021-04-01 16:25:36 -0700

    Added the chess board using a nested for loop to draw out the squares.

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -42,8 +42,55 @@ def main():
     # create game state, gives access to variables in ChessEngine like board for example
     gs = ChessEngine.GameState()
 
-    # example
-    print(gs.board)
+    # example to print chess board
+    # print(gs.board)
+
+    # load in images. do this only once, before the while loop
+    load_images()
+    running = True
+    while running:
+        # clear event queue
+        for e in p.event.get():
+            # to quit game
+            if e.type == p.QUIT:
+                running = False
+        draw_game_state(screen,gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+"""
+Responsible for all the graphics within a current game state.
+"""
+def draw_game_state(screen,gs):
+    # draws the squares on the board
+    draw_board(screen)
+    # draw pieces on top of those squares
+    # can add in piece highlighting or move suggestions later
+    draw_pieces(screen,gs.board)
+
+"""
+Draws the squares on the board. Top left square is always white, bottom right square as well.
+"""
+def draw_board(screen):
+    # two colors picked for chess board, white and grey, not black because the pieces will be hard to see
+    colors = [p.Color("white"), p.Color("grey")]
+    # nested for loop, r = rows / c = columns
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            # gives us the remainder and tells us whether the square is even or odd
+            # white squares are even (remainder 0) , dark squares are odd (remainder 1)
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
-main()
+
+
+
+"""
+Draws the pieces on the board using the current game states board variable (GameState.board)
+"""
+def draw_pieces(screen, board):
+    pass
+
+if __name__ == "__main__":
+    main()
