091b5dd558768172b47f9a4d31e16a0bd6d10a5a
giovannfox <gfox@mail.sfsu.edu>
2021-09-01 11:04:30 -0700

    Add valid king moves

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -180,7 +180,15 @@ class GameState:
     """
 
     def get_king_moves(self, r, c, moves):
-        pass
+        king_moves = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        ally_color = 'w' if self.white_to_move else 'b'
+        for i in range(8):
+            end_row = r + king_moves[i][0]
+            end_col = c + king_moves[i][1]
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != ally_color:  # not an ally piece, empty or enemy piece
+                    moves.append(Move((r, c), (end_row, end_col), self.board))
 
 
 class Move:
